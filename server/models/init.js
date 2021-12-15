508cbe41e1048d0c3e9d4d456f64b1b800d1417e
vivekvppanneerselvam <vivekvppanneerselvam@gmail.com>
2021-12-15 04:37:43 -0800

    slot booking app

--- server/models/init.js
+++ server/models/init.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect(process.env.MONGO_URI, { useMongoClient: true })
-
-  .then(() => {
+/*mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useCreateIndex: true, }).then(() => {
     console.log('Successfully connected to database')
-  })
-
-  .catch(error => {
+  }).catch(error => {
     console.error('Error connecting to MongoDB database', error)
-  })
+  })*/
+  
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, },function(error){
+  if(error)  console.error('Error connecting to MongoDB database', error)
+    console.log(`connect mongodb success`);
+});
 
 module.exports = mongoose
